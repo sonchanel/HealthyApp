515167b155dda2b630fc5d4b99bc2a24b2c0038b
KurobaKaiiiito <shinz141200@gmail.com>
2023-10-23 12:34:26 +0700

    Da connect github

--- HealthyApp/Program.cs
+++ HealthyApp/Program.cs
@@ -8,6 +8,7 @@ namespace HealthyApp
         [STAThread]
         static void Main()
         {
+            //comment: Da ket noi
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
